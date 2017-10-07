d36a906bb6d1e95493713a29e97cc2a4b6b40a86
Christian Bogro <cb@link2b.fr>
2017-10-07 09:10:36 +0000

    Struration de mywork

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -6,9 +6,11 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
+    'Tests\\' => array($baseDir . '/tests'),
     'Psr\\Container\\' => array($vendorDir . '/psr/container/src'),
     'PhpDocReader\\' => array($vendorDir . '/php-di/phpdoc-reader/src/PhpDocReader'),
     'Invoker\\' => array($vendorDir . '/php-di/invoker/src'),
     'Interop\\Container\\' => array($vendorDir . '/container-interop/container-interop/src/Interop/Container'),
     'DI\\' => array($vendorDir . '/php-di/php-di/src/DI'),
+    'App' => array($baseDir . '/app'),
 );
